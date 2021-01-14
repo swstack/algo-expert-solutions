bb8e1bd366fc6b1c18d6642209edc53b32ef5e05
Stephen Stack <sfstack7500@gmail.com>
2021-01-13 18:34:50 -0600

    Solution: ClosestValueInBST

---
+++ src/solutions/easy/ClosestValueInBST.kts
@@ -0,0 +1,30 @@
+import java.lang.Math.abs
+import java.util.Stack
+
+open class BST(value: Int) {
+    var value = value
+    var left: BST? = null
+    var right: BST? = null
+}
+
+fun findClosestValueInBst(tree: BST, target: Int): Int {
+    var stack = Stack<BST>()
+    stack.push(tree)
+
+    var closest = tree.value
+    while (!stack.isEmpty()) {
+        var next = stack.pop()
+        if (next.left != null) {
+            stack.push(next.left)
+        }
+        if (next.right != null) {
+            stack.push(next.right)
+        }
+
+        if (abs(next.value - target) < abs(closest - target)) {
+            closest = next.value
+        }
+    }
+
+    return closest
+}
